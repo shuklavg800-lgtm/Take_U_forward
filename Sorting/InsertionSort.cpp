2817ff322a6e3d6d0aecec5f37c0c339ad3af97e
shuklavg800-lgtm <shuklavg.800@gmail.com>
2025-09-20 23:42:30 +0530

    InsertionSort modified

--- Sorting/InsertionSort.cpp
+++ Sorting/InsertionSort.cpp
@@ -1,13 +1,48 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-void insertionsort(int A[], int n){
-    int i=0;
-    int j=1;
 
+void insertionSort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];   // element to insert
+        int j = i - 1;
 
+        // shift elements greater than key to one position ahead
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
 
+        // insert key at the right position
+        arr[j + 1] = key;
+    }
 }
-int main(int argc, char const *argv[])
-{
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int* arr = new int[n];
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Original array: ";
+    printArray(arr, n);
+
+    insertionSort(arr, n);
+
+    cout << "Sorted array: ";
+    printArray(arr, n);
+
+    delete[] arr;  // free memory
     return 0;
 }
