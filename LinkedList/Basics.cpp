59600aebd6c4ea6a36de90e112f2ac46ea5adc26
Prince Shukla <shuklavg800@gmail.com>
2025-12-25 13:32:46 +0530

    Adding of the conversion function from array to linked list in Basics.cpp

--- LinkedList/Basics.cpp
+++ LinkedList/Basics.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 class Node{
     public:
@@ -10,21 +11,40 @@ class Node{
     }
 };
 
+
+Node* convertArraytoLL(vector<int> & arr){
+    Node* head=new Node(arr[0]);
+    Node* curr=head;
+    for(int i=1;i<arr.size();i++){
+        Node* temp=new Node(arr[i]);
+        curr->next=temp;
+        curr=temp;
+    }
+    return head;
+}
+
 int main(){
     int n=0;
     cout << "Enter the no of nodes you want to have in the linked list: "<< endl;
     cin >> n;
-    Node* head= new Node(10);
-    Node* curr =head;
-    for (int i = 1; i < n; i++)
+    vector<int> temporary(n);
+    for (int i = 0; i < n; i++)
     {
-        int temp_val;
         cout << "Enter the value to be inserted: "<< endl;
-        cin >> temp_val;
-        Node* temp= new Node(temp_val);
-        curr->next=temp;
-        curr=temp;
+        cin >> temporary[i];
     }
+
+    Node* head= convertArraytoLL(temporary);
+    // Node* curr =head;
+    // for (int i = 1; i < n; i++)
+    // {
+    //     int temp_val;
+    //     cout << "Enter the value to be inserted: "<< endl;
+    //     cin >> temp_val;
+    //     Node* temp= new Node(temp_val);
+    //     curr->next=temp;
+    //     curr=temp;
+    // }
     Node* ptr=head;
     while(ptr!=nullptr){
         cout << ptr->data << " ";
