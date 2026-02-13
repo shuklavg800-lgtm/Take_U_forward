0f4a90461c3e5fbfb45bebd0b404ec7c8a17fb34
Prince Shukla <shuklavg800@gmail.com>
2026-02-14 00:29:35 +0530

    Min stack implementation

--- Stack_and_queue/infixTOpostfix.cpp
+++ Stack_and_queue/infixTOpostfix.cpp
@@ -2,6 +2,69 @@
 using namespace std;
 #include <stack>
 
+class infixTOpostfix {
+public:
+
+    int precedence(char c){
+        if(c == '^') return 3;
+        if(c == '*' || c == '/') return 2;
+        if(c == '+' || c == '-') return 1;
+        return -1;
+    }
+
+    string infixToPostfix(string s) {
+        stack<char> st;
+        string ans;
+
+        for(int i = 0; i < s.size(); i++) {
+
+            // If operand, add to result
+            if((s[i] >= 'A' && s[i] <= 'Z') ||
+               (s[i] >= 'a' && s[i] <= 'z') ||
+               (s[i] >= '0' && s[i] <= '9')) {
+                
+                ans += s[i];
+            }
+
+            // If '(', push to stack
+            else if(s[i] == '(') {
+                st.push(s[i]);
+            }
+
+            // If ')', pop until '('
+            else if(s[i] == ')') {
+                while(!st.empty() && st.top() != '(') {
+                    ans += st.top();
+                    st.pop();
+                }
+                st.pop(); // remove '('
+            }
+
+            // Operator case
+            else {
+                // popping elements
+                while(!st.empty() &&
+                     ((precedence(s[i]) < precedence(st.top())) ||
+                     (precedence(s[i]) == precedence(st.top()) && s[i] != '^'))) {
+                    
+                    ans += st.top();
+                    st.pop();
+                }
+                st.push(s[i]);
+            }
+        }
+
+        // Pop remaining operators
+        while(!st.empty()) {
+            ans += st.top();
+            st.pop();
+        }
+
+        return ans;
+    }
+};
+
+
 class MinStack {
 public:
     stack<int> st;
